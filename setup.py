983121cad1fead44c5d6fcca04cce2f7df037051
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-05 12:15:33 +0300

    remove django 1.11 and python 3.4

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ from setuptools import setup
 setup(
     name='ontulily',
     version='0.1',
+    packages=['ontulily'],
     description='A testing tool.',
     license='MIT',
     author='moshthepitt',
     author_email='mosh@mosh.co.ke',
+    url='https://github.com/moshthepitt/ontulily',
     install_requires=[
         'django_micro',
         'requests'
     ],
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
     ],
 )
