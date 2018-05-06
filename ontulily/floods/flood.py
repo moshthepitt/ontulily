ba410a53aca5f595df9cefc46fe13cbb812a8ea3
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-06 14:49:51 +0300

    Add async requests and remove synchronous requests

--- ontulily/floods/flood.py
+++ ontulily/floods/flood.py
@@ -3,75 +3,49 @@ ontulily HTTP Flood module
 
 See: https://github.com/moshthepitt/ontulily/blob/master/docs/flood.md
 """
-import requests
+import concurrent.futures
+from typing import List
+
+from django.conf import settings
+
 from fake_useragent import UserAgent
 from requests.adapters import HTTPAdapter
 from requests.exceptions import RetryError
 from requests.packages.urllib3.util.retry import Retry
 from requests_futures.sessions import FuturesSession
 
-from ontulily.settings import (MAX_WORKERS, REQUESTS_BACKOFF, REQUESTS_RETRIES,
-                               REQUESTS_STATUS_FORCELIST)
-
 USERAGENT = UserAgent()
 
+MAX_WORKERS = settings.MAX_WORKERS
+REQUESTS_BACKOFF = settings.REQUESTS_BACKOFF
+REQUESTS_RETRIES = settings.REQUESTS_RETRIES
+REQUESTS_STATUS_FORCELIST = settings.REQUESTS_STATUS_FORCELIST
 
-def get_request(url: str,
-                user_agent: str=None,
-                proxy: dict=None,
-                params: dict=None,
-                retries: int=REQUESTS_RETRIES,
-                backoff_factor: float=REQUESTS_BACKOFF,
-                status_forcelist: set=REQUESTS_STATUS_FORCELIST):
+
+def package_request(url: str, user_agent: str=None, proxy: dict=None,
+                    params: dict=None, headers: dict=None, method: str='GET',
+                    data: dict=None):
     """
-    Sends a GET request to a URL
+    Prepares a GET request
     """
+
     if user_agent is None:
         user_agent = USERAGENT.random
 
-    headers = {
-        "Connection": "close",  # another way to cover tracks
-        'User-Agent': str(user_agent)}
-
-    result = {'success': False}
-
-    try:
-        r = requests_retry_session(
-            retries=retries, backoff_factor=backoff_factor,
-            status_forcelist=status_forcelist).get(
-                url,
-                proxies=proxy,
-                params=params,
-                headers=headers
-            )
-    except RetryError as e:
-        result['error'] = e
-    except ConnectionError as e:
-        result['error'] = e
+    if headers is None:
+        headers = {
+            "Connection": "close",  # another way to cover tracks
+            'User-Agent': str(user_agent)}
     else:
-        result['success'] = True
-        result['response'] = r
-    finally:
-        return result
-
-
-def requests_retry_session(retries: int=REQUESTS_RETRIES,
-                           backoff_factor: float=REQUESTS_BACKOFF,
-                           status_forcelist: set=REQUESTS_STATUS_FORCELIST,
-                           session: object=None):
-    """
-    A wrapper around requests to enable retrying
-
-    See: https://www.peterbe.com/plog/best-practice-with-retries-with-requests
-    """
-    session = session or requests.Session()
-    retry = Retry(total=retries, read=retries, connect=retries,
-                  backoff_factor=backoff_factor,
-                  status_forcelist=status_forcelist)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
+        headers['User-Agent'] = str(user_agent)
+    return dict(
+        method=method,
+        url=url,
+        params=params,
+        data=data,
+        headers=headers,
+        proxies=proxy
+    )
 
 
 def async_requests_retry_session(
@@ -89,3 +63,64 @@ def async_requests_retry_session(
     session.mount('http://', adapter)
     session.mount('https://', adapter)
     return session
+
+
+def request_url(
+            url: str, user_agent: str=None, proxy: dict=None,
+            params: dict=None, headers: dict=None, method: str='GET',
+            data: dict=None, retries: int=REQUESTS_RETRIES,
+            backoff_factor: float=REQUESTS_BACKOFF,
+            status_forcelist: set=REQUESTS_STATUS_FORCELIST,
+            session: object=None, max_workers: int=MAX_WORKERS):
+    """
+    Sends a request to a url, returns a FutureSession object
+    """
+    the_request = package_request(
+        url=url,
+        user_agent=user_agent,
+        proxy=proxy,
+        params=params,
+        headers=headers,
+        method=method,
+        data=data
+    )
+    the_session = async_requests_retry_session(
+        retries=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+        session=session,
+        max_workers=max_workers
+    )
+    return the_session.request(**the_request)
+
+
+def future_session_handler(future_session: object):
+    """
+    Handler for future session objects
+    """
+    result = {'success': False}
+
+    try:
+        result['response'] = future_session.result()
+    except RetryError as e:
+        result['error'] = e
+    except ConnectionError as e:
+        result['error'] = e
+    else:
+        result['success'] = True
+    finally:
+        return result
+
+
+def quick_request_urls(
+        urls: List[str], proxy: dict=None, params: dict=None, data: dict=None,
+        method: str='GET'):
+    """
+    Shortcut to send the same request to a bunch of urls
+    """
+    futures = (
+        request_url(
+            method=method, url=url, proxy=proxy, params=params, data=data) for
+        url in urls)
+    for future_result in concurrent.futures.as_completed(futures):
+        yield future_session_handler(future_result)
--- ontulily/tests/test_flood.py
+++ ontulily/tests/test_flood.py
@@ -1,68 +1,70 @@
 """
 Tests ontulily.floods.flood.py
 """
-from unittest.mock import patch
-
+from unittest.mock import patch, MagicMock
+import requests_mock
 from django.test import TestCase
 
 from fake_useragent import UserAgent
 from requests.exceptions import RetryError
 
-from ontulily.floods.flood import (async_requests_retry_session, get_request,
-                                   requests_retry_session)
+from ontulily.floods.flood import (async_requests_retry_session,
+                                   future_session_handler, package_request,
+                                   quick_request_urls, request_url)
 
 
 class TestFlood(TestCase):
     """
     Test the methods in flood.py
     """
-    def test_get_request(self):
+    def test_package_request(self):
         """
-        Test that a request to a valid URL actually goes through
+        Test that a package_request resturns as we expect
         """
-        r = get_request(url="https://example.com")
-        self.assertEquals(True, r['success'])
-        self.assertEquals(200, r['response'].status_code)
-
-    @patch('ontulily.floods.flood.requests_retry_session')
-    def test_get_request_params(self, mocked):
-        """
-        Test that get_request passes the expected arguments
-        """
-        proxy = {'http': 'http:127.0.0.1'}
-        params = {'foo': 'bar'}
-        url = 'http://example.com'
         user_agent = UserAgent().chrome
-        headers = {
-            "Connection": "close",  # another way to cover tracks
-            'User-Agent': str(user_agent)}
+        data = dict(
+            url="http://example.com",
+            user_agent=user_agent,
+            proxy={'http': 'http://127.0.0.1'},
+            params={'foo': 'bar'},
+            headers={'Connection': 'close', 'Foo': 'BAR'},
+            method='POST',
+            data={'test': True})
+        expected = dict(
+            url="http://example.com",
+            proxies={'http': 'http://127.0.0.1'},
+            params={'foo': 'bar'},
+            headers={'Connection': 'close', 'Foo': 'BAR',
+                     'User-Agent': user_agent},
+            method='POST',
+            data={'test': True})
+        r = package_request(**data)
+        self.assertDictEqual(expected, r)
 
-        get_request(url=url, user_agent=user_agent, proxy=proxy,
-                    params=params)
+    @patch('ontulily.floods.flood.async_requests_retry_session')
+    def test_request_url(self, mocked):
+        """
+        Test that request_url works
 
-        mocked().get.assert_called_with(
-            url,
-            proxies=proxy,
-            params=params,
-            headers=headers
-        )
-
-    def test_requests_retry_session_success(self):
+        Ensure that request_url calls the `request` method on FutureSession
         """
-        Test that a valid URL can be accessed normally
-        """
-        r = requests_retry_session(retries=0,
-                                   backoff_factor=0).get('http://example.com')
-        self.assertEquals(200, r.status_code)
-
-    def test_requests_retry_session_error(self):
-        """
-        Tests that an invalid URL will eventually fail
-        """
-        with self.assertRaises(RetryError):
-            requests_retry_session(
-                retries=0, backoff_factor=0).get(
-                    'http://httpbin.org/status/500')
+        user_agent = UserAgent().chrome
+        data = dict(
+            url="http://example.com",
+            user_agent=user_agent,
+            proxy={'http': 'http://127.0.0.1'},
+            params={'foo': 'bar'},
+            method='POST',
+            data={'test': True})
+        expected = dict(
+            url="http://example.com",
+            proxies={'http': 'http://127.0.0.1'},
+            params={'foo': 'bar'},
+            headers={'Connection': 'close', 'User-Agent': user_agent},
+            method='POST',
+            data={'test': True})
+        request_url(**data)
+        mocked().request.assert_called_with(**expected)
 
     def test_async_requests_retry_session_success(self):
         """
@@ -78,7 +80,29 @@ class TestFlood(TestCase):
         Tests that an invalid URL will eventually fail using async
         """
         with self.assertRaises(RetryError):
-            future = requests_retry_session(
+            future = async_requests_retry_session(
                 retries=0, backoff_factor=0).get(
                     'http://httpbin.org/status/500')
             future.result()
+
+    def test_future_session_handler(self):
+        """
+        Test future_session_handler
+        """
+        mocked = MagicMock()
+        mocked.result.return_value = 'Noma sana'
+        result = future_session_handler(mocked)
+        self.assertTrue(result['success'])
+        self.assertEquals('Noma sana', result['response'])
+
+    @requests_mock.Mocker()
+    def test_quick_request_urls(self, mocked):
+        """
+        Test quick_request_urls
+        """
+        mocked.get('http://example.com', text='mosh was here')
+        results = quick_request_urls(urls=['http://example.com'])
+        result = list(results)[0]
+        self.assertTrue(result['success'])
+        self.assertEquals(200, result['response'].status_code)
+        self.assertEquals('mosh was here', result['response'].text)
