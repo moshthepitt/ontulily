b937481b23f91bfb4a760272ed2d11ee714d03ab
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-06 17:29:59 +0300

    Fix pylint errors

--- ontulily/floods/flood.py
+++ ontulily/floods/flood.py
@@ -11,7 +11,7 @@ from django.conf import settings
 from fake_useragent import UserAgent
 from requests.adapters import HTTPAdapter
 from requests.exceptions import RetryError
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util import Retry
 from requests_futures.sessions import FuturesSession
 
 USERAGENT = UserAgent()
@@ -22,9 +22,14 @@ REQUESTS_RETRIES = settings.REQUESTS_RETRIES
 REQUESTS_STATUS_FORCELIST = settings.REQUESTS_STATUS_FORCELIST
 
 
-def package_request(url: str, user_agent: str=None, proxy: dict=None,
-                    params: dict=None, headers: dict=None, method: str='GET',
-                    data: dict=None):
+# pylint: disable=too-many-arguments
+def package_request(url: str,
+                    user_agent: str = None,
+                    proxy: dict = None,
+                    params: dict = None,
+                    headers: dict = None,
+                    method: str = 'GET',
+                    data: dict = None):
     """
     Prepares a GET request
     """
@@ -35,7 +40,8 @@ def package_request(url: str, user_agent: str=None, proxy: dict=None,
     if headers is None:
         headers = {
             "Connection": "close",  # another way to cover tracks
-            'User-Agent': str(user_agent)}
+            'User-Agent': str(user_agent)
+        }
     else:
         headers['User-Agent'] = str(user_agent)
     return dict(
@@ -44,34 +50,43 @@ def package_request(url: str, user_agent: str=None, proxy: dict=None,
         params=params,
         data=data,
         headers=headers,
-        proxies=proxy
-    )
+        proxies=proxy)
 
 
 def async_requests_retry_session(
-        retries: int=REQUESTS_RETRIES, backoff_factor: float=REQUESTS_BACKOFF,
-        status_forcelist: set=REQUESTS_STATUS_FORCELIST, session: object=None,
-        max_workers: int=MAX_WORKERS):
+        retries: int = REQUESTS_RETRIES,
+        backoff_factor: float = REQUESTS_BACKOFF,
+        status_forcelist: set = REQUESTS_STATUS_FORCELIST,
+        session: object = None,
+        max_workers: int = MAX_WORKERS):
     """
     A wrapper around requests to enable retrying that uses async requests
     """
-    session = session or FuturesSession(max_workers=MAX_WORKERS)
-    retry = Retry(total=retries, read=retries, connect=retries,
-                  backoff_factor=backoff_factor,
-                  status_forcelist=status_forcelist)
+    session = session or FuturesSession(max_workers=max_workers)
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
     return session
 
 
-def request_url(
-            url: str, user_agent: str=None, proxy: dict=None,
-            params: dict=None, headers: dict=None, method: str='GET',
-            data: dict=None, retries: int=REQUESTS_RETRIES,
-            backoff_factor: float=REQUESTS_BACKOFF,
-            status_forcelist: set=REQUESTS_STATUS_FORCELIST,
-            session: object=None, max_workers: int=MAX_WORKERS):
+def request_url(url: str,
+                user_agent: str = None,
+                proxy: dict = None,
+                params: dict = None,
+                headers: dict = None,
+                method: str = 'GET',
+                data: dict = None,
+                retries: int = REQUESTS_RETRIES,
+                backoff_factor: float = REQUESTS_BACKOFF,
+                status_forcelist: set = REQUESTS_STATUS_FORCELIST,
+                session: object = None,
+                max_workers: int = MAX_WORKERS):
     """
     Sends a request to a url, returns a FutureSession object
     """
@@ -82,15 +97,13 @@ def request_url(
         params=params,
         headers=headers,
         method=method,
-        data=data
-    )
+        data=data)
     the_session = async_requests_retry_session(
         retries=retries,
         backoff_factor=backoff_factor,
         status_forcelist=status_forcelist,
         session=session,
-        max_workers=max_workers
-    )
+        max_workers=max_workers)
     return the_session.request(**the_request)
 
 
@@ -102,25 +115,26 @@ def future_session_handler(future_session: object):
 
     try:
         result['response'] = future_session.result()
-    except RetryError as e:
-        result['error'] = e
-    except ConnectionError as e:
-        result['error'] = e
+    except RetryError as exc:
+        result['error'] = exc
+    except ConnectionError as exc:
+        result['error'] = exc
     else:
         result['success'] = True
-    finally:
-        return result
+
+    return result
 
 
-def quick_request_urls(
-        urls: List[str], proxy: dict=None, params: dict=None, data: dict=None,
-        method: str='GET'):
+def quick_request_urls(urls: List[str],
+                       proxy: dict = None,
+                       params: dict = None,
+                       data: dict = None,
+                       method: str = 'GET'):
     """
     Shortcut to send the same request to a bunch of urls
     """
-    futures = (
-        request_url(
-            method=method, url=url, proxy=proxy, params=params, data=data) for
-        url in urls)
+    futures = (request_url(
+        method=method, url=url, proxy=proxy, params=params, data=data)
+               for url in urls)
     for future_result in concurrent.futures.as_completed(futures):
         yield future_session_handler(future_result)
